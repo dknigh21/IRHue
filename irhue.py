dcb60207810ca1a8393a17e46a5aa5dde0a7a3d9
Daniel Knight <dknigh21@uncc.edu>
2018-12-23 13:24:12 -0500

    Added Settings.ini and changed flag readings
    Added a config file named settings.ini.
    
    Changed the way irHUE compares flags, makes it more readable and easier to program.

--- irhue.py
+++ irhue.py
@@ -1,38 +1,39 @@
 #!python3
 
-import irsdk, time, itertools, sys, socket
-import phue
+import irsdk, time, itertools, sys, socket, configparser
 from phue import Bridge
+
 sys.tracebacklimit = -1
 spinner = itertools.cycle(['-', '/', '|', '\\'])
+
+irsdk_checkered = 0x00000001
+irsdk_white = 0x00000002
+irsdk_green = 0x00000004
+irsdk_yellow = 0x00000008
+irsdk_red = 0x00000010
+irsdk_blue = 0x00000020
+irsdk_debris = 0x00000040
+irsdk_crossed = 0x00000080
+irsdk_yellowWaving = 0x00000100
+irsdk_oneLapToGreen = 0x00000200
+irsdk_greenHeld = 0x00000400
+# irsdk_tenToGo         = 0x00000800; // not displaying this one
+# irsdk_fiveToGo        = 0x00001000; // not displaying this one
+# irsdk_randomWaving    = 0x00002000; // not displaying this one
+irsdk_caution = 0x00004000
+irsdk_cautionWaving = 0x00008000
+irsdk_black = 0x00010000
+irsdk_disqualify = 0x00020000
+# irsdk_servicible      = 0x00040000; // car is allowed service (not a flag)
+irsdk_furled = 0x00080000
+irsdk_repair = 0x00100000
+irsdk_startHidden = 0x10000000
+irsdk_startReady = 0x20000000
+irsdk_startSet = 0x40000000
+irsdk_startGo = 0x80000000
+
 class State:
 	ir_connected = False;
-
-	irsdk_checkered = 0x00000001
-	irsdk_white = 0x00000002
-	irsdk_green = 0x00000004
-	irsdk_yellow = 0x00000008
-	irsdk_red = 0x00000010
-	irsdk_blue = 0x00000020
-	irsdk_debris = 0x00000040
-	irsdk_crossed = 0x00000080
-	irsdk_yellowWaving = 0x00000100
-	irsdk_oneLapToGreen = 0x00000200
-	irsdk_greenHeld = 0x00000400
-	# irsdk_tenToGo         = 0x00000800; // not displaying this one
-	# irsdk_fiveToGo        = 0x00001000; // not displaying this one
-	# irsdk_randomWaving    = 0x00002000; // not displaying this one
-	irsdk_caution = 0x00004000
-	irsdk_cautionWaving = 0x00008000
-	irsdk_black = 0x00010000
-	irsdk_disqualify = 0x00020000
-	# irsdk_servicible      = 0x00040000; // car is allowed service (not a flag)
-	irsdk_furled = 0x00080000
-	irsdk_repair = 0x00100000
-	irsdk_startHidden = 0x10000000
-	irsdk_startReady = 0x20000000
-	irsdk_startSet = 0x40000000
-	irsdk_startGo = 0x80000000
 	
 def check_iracing():
     if state.ir_connected and not (ir.is_initialized and ir.is_connected):
@@ -61,32 +62,28 @@ def lights_off():
 	for l in lights:
 		l.brightness = 1
 		
-def main_loop():
+def main_loop(blink, dim):
 	
-	prev_flag = ''
-	flags = hex(ir['SessionFlags'])
-	sys.stdout.write(flags)
+	prev_flag = 0
+	flag = int(str(ir['SessionFlags']), 16)
+	sys.stdout.write(str(flag))
 	sys.stdout.flush()
-	sys.stdout.write("\b\b\b\b\b\b\b\b\b\b")
+	sys.stdout.write("\b\b\b\b\b\b\b\b\b\b\b")
 	
-	if not flags == prev_flag:
-		if flags[9] == '4':
+	if not flag == prev_flag:
+		if flag & (irsdk_green or irsdk_greenHeld):
 			set_color(25500, 200)
-		elif flags[9] == '2':
+		elif flag & irsdk_white:
 			set_color(11111, 0)
-		elif (flags[6] == '8' or flags[6] == '4') and (flags[7] == '0'):
+		elif flag & (irsdk_yellow or irsdk_yellowWaving or irsdk_caution or irsdk_cautionWaving):
 			set_color(11200, 200)
-			caution_loop()
+			if blink == 1:
+				caution_loop()
 		else:
-			lights_off()
+			if dim == 1:
+				lights_off()
 	
-	
-#0x10000000
-#268435456 = green flag racing (pedal loop)
-
-#0x10000004
-#268435460 = green flag start (all green)
-	
+		
 def caution_loop():
 	
 		for l in lights:
@@ -100,15 +97,22 @@ def caution_loop():
 
 if __name__ == '__main__':
 	
+	config = configparser.ConfigParser()
+	config.read('settings.ini')
 	ir = irsdk.IRSDK(parse_yaml_async=True)
 	state = State()
 	sleep_time = 0.1
+	blink = config['DEFAULT']['BlinkCautionLights']
+	dim = config['DEFAULT']['DimLights']
 	
 	print("\nWelcome to irHUE: A small python application for controlling Phillips Hue lights via iRacing.\nPress ctrl-c at any time to exit the program\n")
 	
 	while True:
 	
-		ip = input("Enter Bridge IP address: ")
+		if len(config['DEFAULT']['BridgeIP']) == 0:
+			ip = input("No default IP specified!\nEnter Bridge IP address: ")
+		else:
+			ip = config['DEFAULT']['BridgeIP']
 
 		print("Attempting bridge connection...")
 	
@@ -131,7 +135,7 @@ if __name__ == '__main__':
 			if state.ir_connected:
 				
 				sleep_time = 1
-				main_loop()
+				main_loop(blink, dim)
 				
 			else:
 				sys.stdout.write("Waiting for iRacing ")
